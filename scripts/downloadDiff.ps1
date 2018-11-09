003e0c82fdc795987da65fb958a775b1d31db56f
Michael Mainer <mmainer@microsoft.com>
2018-11-09 15:46:16 -0800

    update

--- scripts/downloadDiff.ps1
+++ scripts/downloadDiff.ps1
@@ -1,21 +1,18 @@
 ﻿# This script is intended to be run from the root of the Graph_Metadata repo.
 
-param([String]$endpointVersion)
+param([parameter(Mandatory=$true)][String]$endpointVersion)
 
 # Contains Format-Xml function.
 Import-Module .\scripts\utility.ps1 -Force
 
-if ($endpointVersion -eq 'v1.0' -or $endpointVersion -eq 'beta') {
-} else { $endpointVersion = 'v1.0' }
-
 # Download the metadata from livesite. 
 $url = "http://graph.microsoft.com/{0}/`$metadata" -f $endpointVersion
 $metadataFileName = "{0}_metadata.xml" -f $endpointVersion
-$pathToLiveMetadata = "D:\repos\Graph_Metadata\{0}" -f $metadataFileName # TODO: get file location via build env variable.
+$pathToLiveMetadata = "{0}\{1}" -f ($pwd).path, $metadataFileName
 $client = new-object System.Net.WebClient
 $client.Encoding = [System.Text.Encoding]::UTF8
 $client.DownloadFile($url, $pathToLiveMetadata)
-Write-Host "Downloaded metadata from $url" -ForegroundColor DarkGreen
+Write-Host "Downloaded metadata from $url to $pathToLiveMetadata" -ForegroundColor DarkGreen
 
 # Format the metadata to make it easy for us hoomans to read and perform non-tag line based diffs.
 $content = Format-Xml (Get-Content $pathToLiveMetadata) 
